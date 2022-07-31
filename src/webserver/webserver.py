8e2f23853bc6ca30205ea0626541dd2e9700a8ce
KEN_2000 <79527273+ken-2000l@users.noreply.github.com>
2022-07-31 22:50:35 +0400

    fix external ip not set in html

--- src/webserver/webserver.py
+++ src/webserver/webserver.py
@@ -5,26 +5,6 @@ from http.server import BaseHTTPRequestHandler
 from socketserver import ThreadingTCPServer
 from threading import Thread
 
-websocket_uri = 'ws://localhost:6969'
-
-with open(f'{os.path.dirname(__file__)}/index.html', 'r') as f:
-    template_html = f.read().replace("'ws://localhost:6969'", f'\'{websocket_uri}\'')
-
-
-class Handler(BaseHTTPRequestHandler):
-    """handler that serves a static page when accessed from the root url,
-    and sends 404 otherwise"""
-    # noinspection PyPep8Naming
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html; charset=utf-8')
-            self.send_header('Content-length', str(len(template_html)))
-            self.end_headers()
-            self.wfile.write(template_html.encode('utf-8'))
-        else:
-            self.send_error(404)
-
 
 class WebServer(ThreadingTCPServer):
     """same as socketserver.ThreadingTCPServer but with a timeout"""
@@ -37,12 +17,29 @@ class WebServer(ThreadingTCPServer):
 
 
 def run(websocket_ip: str = None, websocket_port: int = 6969, webserver_port: int = 8080):
-    global websocket_uri
     print('Starting Web Server')
 
     if websocket_ip is None:
         websocket_ip = urlopen('https://api.ipify.org/').read().decode('utf8')
     websocket_uri = f'ws://{websocket_ip}:{websocket_port}'
+
+    with open(f'{os.path.dirname(__file__)}/index.html', 'r') as f:
+        template_html = f.read().replace("'ws://localhost:6969'", f'\'{websocket_uri}\'')
+
+    class Handler(BaseHTTPRequestHandler):
+        """handler that serves a static page when accessed from the root url,
+        and sends 404 otherwise"""
+
+        # noinspection PyPep8Naming
+        def do_GET(self):
+            if self.path == '/':
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html; charset=utf-8')
+                self.send_header('Content-length', str(len(template_html)))
+                self.end_headers()
+                self.wfile.write(template_html.encode('utf-8'))
+            else:
+                self.send_error(404)
 
     server = WebServer(('', webserver_port), Handler)
     server_thread = Thread(target=server.serve_forever)
